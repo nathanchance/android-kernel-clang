1e27ed29a9f720c26eaaa21f140a003578b6e183
Greg Hackmann <ghackmann@google.com>
2016-12-13 10:39:45 -0800

    ANDROID: HACK: futex, LLVMLinux: don't optimize futex_detect_cmpxchg() on ARM64
    On ARM64, clang may propagate the uaddr = NULL constant all the way from
    futex_detect_cmpxchg() to futex_atomic_cmpxchg_inatomic(), and try to
    fetch from [xzr] (which there's no way to assemble).
    
    Prevent this by disabling optimizations in futex_detect_cmpxchg().  This
    is ugly, but it only impacts a function that runs once at boot.
    
    Change-Id: I46276159a97d62c03a0719620ebc69642df87fb3
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -3281,6 +3281,9 @@ SYSCALL_DEFINE6(futex, u32 __user *, uaddr, int, op, u32, val,
 }
 
 static void __init futex_detect_cmpxchg(void)
+#if defined(__clang__) && IS_ENABLED(CONFIG_ARM64)
+__attribute__((optnone))
+#endif
 {
 #ifndef CONFIG_HAVE_FUTEX_CMPXCHG
 	u32 curval;
