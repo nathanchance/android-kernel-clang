75bb1fc4bf3a76593a517edc39a59c8725c81341
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-08-03 12:35:52 -0700

    cfg80211: Support backport of removing ieee80211
    Bug: 62058353
    Change-Id: Id8725947048bb4ba461dcb77b7b9023991a304be
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Link: https://android.googlesource.com/kernel/msm/+/2a114e175223e034ce536c81506d89baf50db264
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -5921,4 +5921,11 @@ void cfg80211_update_owe_info_event(struct net_device *netdev,
 				    struct cfg80211_update_owe_info *owe_info,
 				    gfp_t gfp);
 
+/* Due to our tree having a backport of
+ * 57fbcce37be7c1d2622b56587c10ade00e96afa3, this allows QC to support 4.7+
+ * kernels that use the newer NL80211_BAND_* and older kernels that use the
+ * older IEEE80211_BAND_* enums.
+ */
+#define CFG80211_REMOVE_IEEE80211_BACKPORT 1
+
 #endif /* __NET_CFG80211_H */
