f1aad84ec725f794e490d5fd7ffdaa1ada3194b3
Nathan Chancellor <natechancellor@gmail.com>
2018-05-23 18:54:13 -0700

    sched/core: Avoid a constant logical operand warning
    When SCHED_DEBUG is disabled, sched_feat evaluates to a constant,
    which triggers a Clang warning:
    
    kernel/sched/core.c:935:31: error: use of logical '&&' with constant operand [-Werror,-Wconstant-logical-operand]
    
    The kernel developers explicitly state the way sched_feat is setup
    is to take advantage of compiler options. From kernel/sched/features.h:
    
    "Each translation unit has its own copy of sysctl_sched_features to
    allow constants propagation at compile time and compiler optimization
    based on features default."
    
    Clearly, Clang doesn't optimize this like GCC does. Make both compilers
    happy with a static inline function like the one that was added for
    ENERGY_AWARE in http://github.com/aosp-mirror/kernel_common/commit/e496f328c87173fe188e91c6cd60197fb8e7e305.
    
    We do lose a little bit of optimization but that is alright as it is
    only in this one place.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -807,6 +807,11 @@ void deactivate_task(struct rq *rq, struct task_struct *p, int flags)
 	dequeue_task(rq, p, flags);
 }
 
+static inline bool nontask_capacity(void)
+{
+	return sched_feat(NONTASK_CAPACITY);
+}
+
 static void update_rq_clock_task(struct rq *rq, s64 delta)
 {
 /*
@@ -856,7 +861,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 	rq->clock_task += delta;
 
 #if defined(CONFIG_IRQ_TIME_ACCOUNTING) || defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
-	if ((irq_delta + steal) && sched_feat(NONTASK_CAPACITY))
+	if ((irq_delta + steal) && nontask_capacity())
 		sched_rt_avg_update(rq, irq_delta + steal);
 #endif
 }
