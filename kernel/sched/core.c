b8198c58185d851a073bdaf5b7ca73567a7314a9
Sultan Alsawaf <sultanxda@gmail.com>
2018-08-18 12:05:17 -0700

    sched/core: Properly fix constant logical operand Clang warning
    When sched_feat(NONTASK_CAPACITY) evaluates to a constant at compile time,
    Clang is unhappy. Use a double negation to make the constant boolean so
    that Clang is happy.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -858,7 +858,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 	rq->clock_task += delta;
 
 #if defined(CONFIG_IRQ_TIME_ACCOUNTING) || defined(CONFIG_PARAVIRT_TIME_ACCOUNTING)
-	if ((irq_delta + steal) && sched_feat(NONTASK_CAPACITY))
+	if ((irq_delta + steal) && !!sched_feat(NONTASK_CAPACITY))
 		sched_rt_avg_update(rq, irq_delta + steal);
 #endif
 }
