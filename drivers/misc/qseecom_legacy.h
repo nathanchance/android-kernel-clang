aeba82f09493d657178d15e9f29a141270e43cf8
Anmolpreet Kaur <anmolpre@codeaurora.org>
2019-03-06 14:18:06 +0530

    qseecom: Fix clang compilation in qseecom driver
    Make changes to ensure qseecom driver compiles
    fine with clang enabled.
    
    Change-Id: I390a1438425d77c23438fceb5c2c52063b472c90
    Signed-off-by: Anmolpreet Kaur <anmolpre@codeaurora.org>
    [nc: Also apply the same fix to drivers/misc/qseecom_legacy.h, which
         does not exist in 4.9]
    Link: https://source.codeaurora.org/quic/la/kernel/msm-4.9/commit/?id=dd006fea3236d1d5b690c30af84d7e25db092e43
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/misc/qseecom_legacy.h
+++ drivers/misc/qseecom_legacy.h
@@ -33,7 +33,7 @@ enum tz_sched_cmd_status {
 	TZ_SCHED_STATUS_MAX  = 0x7FFFFFFF
 };
 /* Command structure for initializing shared buffers */
-__packed struct qse_pr_init_sb_req_s {
+struct qse_pr_init_sb_req_s {
 	/* First 4 bytes should always be command id */
 	uint32_t                  pr_cmd;
 	/* Pointer to the physical location of sb buffer */
@@ -41,14 +41,14 @@ __packed struct qse_pr_init_sb_req_s {
 	/* length of shared buffer */
 	uint32_t                  sb_len;
 	uint32_t                  listener_id;
-};
+} __attribute__((__packed__));
 
-__packed struct qse_pr_init_sb_rsp_s {
+struct qse_pr_init_sb_rsp_s {
 	/* First 4 bytes should always be command id */
 	uint32_t                  pr_cmd;
 	/* Return code, 0 for success, Approp error code otherwise */
 	int32_t                   ret;
-};
+} __attribute__((__packed__));
 
 /*
  * struct QSEECom_command - QSECom command buffer
@@ -57,11 +57,11 @@ __packed struct qse_pr_init_sb_rsp_s {
  *                buffer
  * @sb_in_cmd_len: length of command buffer
  */
-__packed struct qseecom_command {
+struct qseecom_command {
 	uint32_t               cmd_type;
 	uint8_t                *sb_in_cmd_addr;
 	uint32_t               sb_in_cmd_len;
-};
+} __attribute__((__packed__));
 
 /*
  * struct QSEECom_response - QSECom response buffer
@@ -70,10 +70,10 @@ __packed struct qseecom_command {
  *                buffer
  * @sb_in_rsp_len: length of command response
  */
-__packed struct qseecom_response {
+struct qseecom_response {
 	uint32_t                 cmd_status;
 	uint8_t                  *sb_in_rsp_addr;
 	uint32_t                 sb_in_rsp_len;
-};
+} __attribute__((__packed__));
 
 #endif /* __QSEECOM_LEGACY_H_ */
