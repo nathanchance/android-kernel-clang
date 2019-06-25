6dfdaaa15ee1f1143da7a83ef97932eba13bca58
Nathan Chancellor <natechancellor@gmail.com>
2019-06-24 19:51:31 -0700

    input: touchscren: st: Remove unnecessary parentheses
    Clang warns:
    
    ../drivers/input/touchscreen/st/fts_gui.c:61:10: warning: equality
    comparison with extraneous parentheses [-Wparentheses-equality]
                    if ((i == 0)) {
                         ~~^~~~
    ../drivers/input/touchscreen/st/fts_gui.c:61:10: note: remove extraneous
    parentheses around the comparison to silence this warning
                    if ((i == 0)) {
                        ~  ^   ~
    ../drivers/input/touchscreen/st/fts_gui.c:61:10: note: use '=' to turn
    this equality comparison into an assignment
                    if ((i == 0)) {
                           ^~
                           =
    ../drivers/input/touchscreen/st/fts_gui.c:128:10: warning: equality
    comparison with extraneous parentheses [-Wparentheses-equality]
                    if ((i == 0)) {
                         ~~^~~~
    ../drivers/input/touchscreen/st/fts_gui.c:128:10: note: remove
    extraneous parentheses around the comparison to silence this warning
                    if ((i == 0)) {
                        ~  ^   ~
    ../drivers/input/touchscreen/st/fts_gui.c:128:10: note: use '=' to turn
    this equality comparison into an assignment
                    if ((i == 0)) {
                           ^~
                           =
    ../drivers/input/touchscreen/st/fts_gui.c:176:10: warning: equality
    comparison with extraneous parentheses [-Wparentheses-equality]
                    if ((i == 0)) {
                         ~~^~~~
    ../drivers/input/touchscreen/st/fts_gui.c:176:10: note: remove
    extraneous parentheses around the comparison to silence this warning
                    if ((i == 0)) {
                        ~  ^   ~
    ../drivers/input/touchscreen/st/fts_gui.c:176:10: note: use '=' to turn
    this equality comparison into an assignment
                    if ((i == 0)) {
                           ^~
                           =
    ../drivers/input/touchscreen/st/fts_gui.c:244:10: warning: equality
    comparison with extraneous parentheses [-Wparentheses-equality]
                    if ((i == 0)) {
                         ~~^~~~
    ../drivers/input/touchscreen/st/fts_gui.c:244:10: note: remove
    extraneous parentheses around the comparison to silence this warning
                    if ((i == 0)) {
                        ~  ^   ~
    ../drivers/input/touchscreen/st/fts_gui.c:244:10: note: use '=' to turn
    this equality comparison into an assignment
                    if ((i == 0)) {
                           ^~
                           =
    4 warnings generated.
    
    Fixes: ed29ea5a01b9 ("input: touchpanel: Add ST Touchscreen version 4.1.0 driver")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/input/touchscreen/st/fts_gui.c
+++ drivers/input/touchscreen/st/fts_gui.c
@@ -58,7 +58,7 @@ ssize_t fts_i2c_wr_show(struct device *dev, struct device_attribute *attr,
 	snprintf(buff, sizeof(buff), "{");
 	strlcat(Out_buff, buff,  ARRAY_SIZE(Out_buff));
 	for (i = 0; i < (byte_count_read+2); i++) {
-		if ((i == 0)) {
+		if (!i) {
 			char temp_byte_count_read = (byte_count_read >> 8) & 0xFF;
 			snprintf(buff, sizeof(buff), "%02X", temp_byte_count_read);
 		} else if (i == 1) {
@@ -125,7 +125,7 @@ ssize_t fts_i2c_wr_store(struct device *dev, struct device_attribute *attr,
 #ifdef SCRIPTLESS_DEBUG
 	printk("%s:DATA READ\n{", __func__);
 	for (i = 0; i < (2+byte_count_read); i++) {
-		if ((i == 0)) {
+		if (!i) {
 			char temp_byte_count_read = (byte_count_read >> 8) & 0xFF;
 			printk("%02X", (unsigned int)temp_byte_count_read);
 		} else if (i == 1) {
@@ -173,7 +173,7 @@ ssize_t fts_i2c_read_show(struct device *dev, struct device_attribute *attr,
 	snprintf(buff, sizeof(buff), "{");
 	strlcat(Out_buff, buff,  ARRAY_SIZE(Out_buff));
 	for (i = 0; i < (byte_count_read+2); i++) {
-		if ((i == 0)) {
+		if (!i) {
 			char temp_byte_count_read = (byte_count_read >> 8) & 0xFF;
 			snprintf(buff, sizeof(buff), "%02X", temp_byte_count_read);
 		} else if (i == 1) {
@@ -241,7 +241,7 @@ ssize_t fts_i2c_read_store(struct device *dev, struct device_attribute *attr,
 #ifdef SCRIPTLESS_DEBUG
 	printk("%s:DATA READ\n{", __func__);
 	for (i = 0; i < (byte_count_read+2); i++) {
-		if ((i == 0)) {
+		if (!i) {
 			char temp_byte_count_read = (byte_count_read >> 8) & 0xFF;
 			printk("%02X", (unsigned int)temp_byte_count_read);
 		} else if (i == 1) {
