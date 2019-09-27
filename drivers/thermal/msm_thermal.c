fbedda2fb464f3d865f0e7db9bc158f35e24b4e1
Nathan Chancellor <natechancellor@gmail.com>
2019-09-26 23:47:42 -0700

    msm: thermal: Fix strlcpy usage in fetch_cpu_mitigaiton_info
    Clang warns:
    
    ../drivers/thermal/msm_thermal.c:6355:11: warning: size argument in
    'strlcpy' call appears to be size of the source; expected the size of
    the destination [-Wstrlcpy-strlcat-size]
                            strlen(sensor_name) + 1);
                            ~~~~~~~^~~~~~~~~~~~~~~~
    
    Fixes: b68798fafa4e ("soc: qcom: Snapshot of thermal/LMH drivers")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -6351,8 +6351,8 @@ static int fetch_cpu_mitigaiton_info(struct msm_thermal_data *data,
 			err = -ENOMEM;
 			goto fetch_mitig_exit;
 		}
-		strlcpy((char *) cpus[_cpu].sensor_type, sensor_name,
-			strlen(sensor_name) + 1);
+		strscpy((char *)cpus[_cpu].sensor_type, sensor_name,
+			sizeof(cpus[_cpu].sensor_type));
 		create_alias_name(_cpu, limits, pdev);
 	}
 
