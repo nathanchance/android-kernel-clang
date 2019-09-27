720ebe73d99b9ba0d3d4324e0539eb4c244fe090
Nathan Chancellor <natechancellor@gmail.com>
2019-09-26 21:31:32 -0700

    clk: qcom: clk-debug: Reorder if statement to avoid self assignment
    Currently, this if statement is worded as:
    
    if (a == 0xFF)
        b = b;
    else if (a) /* a != 0 and a != 0xFF */
        b |= c;
    else /* a == 0 */
        b |= d;
    
    This causes a clang warning:
    
    ../drivers/clk/qcom/clk-debug.c:182:11: warning: explicitly assigning
    value of variable of type 'u32' (aka 'unsigned int') to itself
    [-Wself-assign]
                            regval = regval;
                            ~~~~~~ ^ ~~~~~~
    1 warning generated.
    
    This can be reworded as:
    
    if (!a) /* a == 0 */
        b |= d;
    else if (a != 0xFF)
        b |= c;
    
    which is equivalent in meaning and avoids the warning.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/clk/qcom/clk-debug.c
+++ drivers/clk/qcom/clk-debug.c
@@ -177,13 +177,10 @@ static int clk_debug_mux_set_parent(struct clk_hw *hw, u8 index)
 
 		regval |= (meas->parent[index].next_sel & meas->mask);
 
-		if (meas->parent[index].en_mask == 0xFF)
-			/* Skip en_mask */
-			regval = regval;
-		else if (meas->parent[index].en_mask)
-			regval |= meas->parent[index].en_mask;
-		else
+		if (!meas->parent[index].en_mask)
 			regval |= meas->en_mask;
+		else if (meas->parent[index].en_mask != 0xFF)
+			regval |= meas->parent[index].en_mask;
 
 		regmap_write(meas->regmap[dbg_cc], 0x0, regval);
 	}
