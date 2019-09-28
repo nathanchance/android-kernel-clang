fa6670529c693ee0d093ebde12f45e1449dbd6c2
Nathan Chancellor <natechancellor@gmail.com>
2019-09-28 14:02:20 -0700

    ASoC: msm-pcm-voice-v2: Fix location of second const qualifier for  ty_mode
    Clang warns:
    
    ../sound/soc/msm/qdsp6v2/msm-pcm-voice-v2.c:497:19: warning: duplicate
    'const' declaration specifier [-Wduplicate-decl-specifier]
    static const char const *tty_mode[] = {"OFF", "HCO", "VCO", "FULL"};
                      ^~~~~~
    1 warning generated.
    
    Fixes: a886cfa6d2d7 ("ASoC: add audio drivers for MSM targets")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- sound/soc/msm/qdsp6v2/msm-pcm-voice-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-voice-v2.c
@@ -494,7 +494,7 @@ done:
 
 
 
-static const char const *tty_mode[] = {"OFF", "HCO", "VCO", "FULL"};
+static const char * const tty_mode[] = {"OFF", "HCO", "VCO", "FULL"};
 static const struct soc_enum msm_tty_mode_enum[] = {
 		SOC_ENUM_SINGLE_EXT(4, tty_mode),
 };
